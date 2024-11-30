20e3c084553a52a743cfa2ee307e823810999d70
camaranto <camaranto@uninorte.edu.co>
2024-11-29 23:53:10 -0500

    Basic rasterization, not working

--- src/DemoViewer.java
+++ src/DemoViewer.java
@@ -4,6 +4,7 @@ import java.awt.Container;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Path2D;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
@@ -32,44 +33,44 @@ class DemoViewer {
 
                 // rendering
                 double size = 100; 
-                ArrayList<Square> squares = new ArrayList<Square>();
-                squares.add(new Square(new Vertex(size, size, size), 
-                                    new Vertex(-size, size, size), 
-                                    new Vertex(-size, -size, size), 
-                                    new Vertex(size, -size, size), 
-                                    Color.WHITE));
-                squares.add(new Square(new Vertex(size, size, -size), 
-                                    new Vertex(-size, size, -size), 
-                                    new Vertex(-size, -size, -size), 
-                                    new Vertex(size, -size, -size), 
-                                    Color.RED));
-                squares.add(new Square(new Vertex(size, size, size), 
-                                    new Vertex(size, size, -size), 
-                                    new Vertex(-size, size, -size), 
-                                    new Vertex(-size, size, size), 
-                                    Color.GREEN));
-                squares.add(new Square(new Vertex(size, -size, size), 
-                                    new Vertex(size, -size, -size), 
-                                    new Vertex(-size, -size, -size), 
-                                    new Vertex(-size, -size, size), 
-                                    Color.BLUE));
-                // ArrayList<Triangle> tris = new ArrayList<Triangle>();
-                // tris.add(new Triangle(new Vertex(size, size, size),
-                //                 new Vertex(-size, -size, size),
-                //                 new Vertex(-size, size, -size),
-                //                 Color.WHITE));
-                // tris.add(new Triangle(new Vertex(size, size, size),
-                //                 new Vertex(-size, -size, size),
-                //                 new Vertex(size, -size, -size),
-                //                 Color.RED));
-                // tris.add(new Triangle(new Vertex(-size, size, -size),
-                //                 new Vertex(size, -size, -size),
-                //                 new Vertex(size, size, size),
-                //                 Color.GREEN));
-                // tris.add(new Triangle(new Vertex(-size, size, -size),
-                //                 new Vertex(size, -size, -size),
-                //                 new Vertex(-size, -size, size),
-                //                 Color.BLUE));
+                // ArrayList<Square> squares = new ArrayList<Square>();
+                // squares.add(new Square(new Vertex(size, size, size), 
+                //                     new Vertex(-size, size, size), 
+                //                     new Vertex(-size, -size, size), 
+                //                     new Vertex(size, -size, size), 
+                //                     Color.WHITE));
+                // squares.add(new Square(new Vertex(size, size, -size), 
+                //                     new Vertex(-size, size, -size), 
+                //                     new Vertex(-size, -size, -size), 
+                //                     new Vertex(size, -size, -size), 
+                //                     Color.RED));
+                // squares.add(new Square(new Vertex(size, size, size), 
+                //                     new Vertex(size, size, -size), 
+                //                     new Vertex(-size, size, -size), 
+                //                     new Vertex(-size, size, size), 
+                //                     Color.GREEN));
+                // squares.add(new Square(new Vertex(size, -size, size), 
+                //                     new Vertex(size, -size, -size), 
+                //                     new Vertex(-size, -size, -size), 
+                //                     new Vertex(-size, -size, size), 
+                //                     Color.BLUE));
+                ArrayList<Triangle> tris = new ArrayList<Triangle>();
+                tris.add(new Triangle(new Vertex(size, size, size),
+                                new Vertex(-size, -size, size),
+                                new Vertex(-size, size, -size),
+                                Color.WHITE));
+                tris.add(new Triangle(new Vertex(size, size, size),
+                                new Vertex(-size, -size, size),
+                                new Vertex(size, -size, -size),
+                                Color.RED));
+                tris.add(new Triangle(new Vertex(-size, size, -size),
+                                new Vertex(size, -size, -size),
+                                new Vertex(size, size, size),
+                                Color.GREEN));
+                tris.add(new Triangle(new Vertex(-size, size, -size),
+                                new Vertex(size, -size, -size),
+                                new Vertex(-size, -size, size),
+                                Color.BLUE));
                 // g2.translate(getWidth()/2, getHeight()/2);
                 // g2.setColor(Color.WHITE);
                 // for (Triangle t : tris) {
@@ -85,25 +86,50 @@ class DemoViewer {
                 Matrix3 pitchTransform = Matrix3.MatrixYZ(Math.toRadians(pitchSlider.getValue()));
                 Matrix3 transform = headingTransform.multiply(pitchTransform);
 
-                //BufferedImage img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
+                BufferedImage img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
 
-                g2.translate(getWidth() / 2, getHeight() / 2);
-                g2.setColor(Color.WHITE);
-                for (Square s : squares) {
-                    Vertex v1 = transform.transform(s.v1);
-                    Vertex v2 = transform.transform(s.v2);
-                    Vertex v3 = transform.transform(s.v3);
-                    Vertex v4 = transform.transform(s.v4);
+                // g2.translate(getWidth() / 2, getHeight() / 2);
+                // g2.setColor(Color.WHITE);
+                for (Triangle t : tris) {
+                    Vertex v1 = transform.transform(t.v1);
+                    Vertex v2 = transform.transform(t.v2);
+                    Vertex v3 = transform.transform(t.v3);
+
+                    v1.x += getWidth() / 2;
+                    v1.y += getHeight() / 2;
+                    v2.x += getWidth() / 2;
+                    v2.y += getHeight() / 2;                    
+                    v3.x += getWidth() / 2;
+                    v3.y += getHeight() / 2;
+
+                    int minX = (int) Math.max(0, Math.ceil(Math.min(v1.x, Math.min(v2.x, v3.x))));
+                    int maxX = (int) Math.min(img.getWidth() - 1, Math.floor(Math.max(v1.x, Math.max(v2.x, v3.x))));
+
+                    int minY = (int) Math.max(0, Math.ceil(Math.min(v1.y, Math.min(v2.y, v3.y))));
+                    int maxY = (int) Math.min(img.getHeight() -1, Math.floor(Math.max(v1.y, Math.max(v2.y, v3.y))));
+
+                    double triangleArea = (v1.y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - v1.x);
+
+                    for (int y = minY; y <= maxY; y++) {
+                        for (int x = minX; x <=  maxX; x++) {
+                            double b1 = ((y - v3.y) * (v2.x - v3.x) + (v2.y + v3.y) * (v3.x -x)) / triangleArea;
+                            double b2 = ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x -x)) / triangleArea;
+                            double b3 = ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x -x)) / triangleArea;
+                            if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1 ) {
+                                img.setRGB(x, y, t.color.getRGB());
+                            }
+                        }
+
+                    }
+
+                    // Path2D path = new Path2D.Double();
+                    // path.moveTo(v1.x, v1.y);
+                    // path.lineTo(v2.x, v2.y);
+                    // path.lineTo(v3.x, v3.y);
+                    // path.closePath();
                     
-
-                    Path2D path = new Path2D.Double();
-                    path.moveTo(v1.x, v1.y);
-                    path.lineTo(v2.x, v2.y);
-                    path.lineTo(v3.x, v3.y);
-                    path.lineTo(v4.x, v4.y);
-                    path.closePath();
-                    g2.draw(path);
                 }
+                g2.drawImage(img, 0, 0, null);
                 
             }
         };
